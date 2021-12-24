4bd5c0b70d4280db39d01d6d2edd96de012c5615
Shashank Dubey <jfriday464@gmail.com>
2021-12-24 12:12:47 +0530

    Binary Search done

--- Searching-Algorithms/BinarySeach.c
+++ Searching-Algorithms/BinarySeach.c
@@ -1,24 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
 
-#define MAX 7;
+int binarySearch (int arr[], int size, int search ){
+    int low = 0, high = arr[size - 1];
+    int mid = (low + high) / 2;
 
-void arrayTraversal (int arr[]){
-    for(int i = 0; i< sizeof(arr)/ sizeof(arr[0]); i++ ){
-        printf("%d, ",arr[i]);
+    while(high >= low){
+        if(arr[mid] == search){
+            return mid;
+        }
+
+        if(arr[mid] < search)
+            low = mid + 1;
+        else
+            high = mid - 1;
+
+        mid = (low + high) / 2;
     }
+    return -1;
 }
 
 int main(void){
-    int array[] = {2,3,4,5,6,7,8},i = 0;
+    int arr[] = {2,3,4,5,6,7,8}, search = 2;
+    size_t size = sizeof(arr) / sizeof(arr[0]);
 
-    // while (i < sizeof(array)/ sizeof(array[0]) )
-    // {
-    //     printf("%d\n",array[i]);
-    //     i++;
-    // }
-    arrayTraversal(array);
-    
+    int index = binarySearch(arr, size, search);
+
+    (index == - 1) 
+        ? printf("%d Not Found!",search)
+        : printf("%d found at index position %d",search,index);
 
     return 0;
 }
